70dd91fe9663377e56755003ac7b8672e243e19b
Dan Walkes <danwalkes@trellis-logic.com>
2022-08-18 13:28:30 -0600

    Initial implementation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # nvidia-ota-scripts
 
-Scripts to (attempt to) automate the steps in the [NVIDIA Developer guide](https://docs.nvidia.com/jetson/archives/r35.1/DeveloperGuide/text/SD/SoftwarePackagesAndTheUpdateMechanism.html#preparing-for-an-image-based-ota-update)
+Scripts to automate the steps in the [NVIDIA Developer guide](https://docs.nvidia.com/jetson/archives/r35.1/DeveloperGuide/text/SD/SoftwarePackagesAndTheUpdateMechanism.html#preparing-for-an-image-based-ota-update)
 related to image based OTA updates.
 
+These are currently hard coded to demonstrate an upgrade for `jetson-xavier-nx-devkit` from Jetpack 4.6 to Jetpak 5.0
--- setup-host-machine.sh
+++ setup-host-machine.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # See instructions at https://docs.nvidia.com/jetson/archives/r35.1/DeveloperGuide/text/SD/SoftwarePackagesAndTheUpdateMechanism.html#preparing-for-an-image-based-ota-update
-base_release="34.1.1"
+base_release="32.7.2"
 # See format from https://docs.nvidia.com/jetson/archives/r35.1/DeveloperGuide/text/SD/SoftwarePackagesAndTheUpdateMechanism.html#updating-jetson-linux-with-image-based-over-the-air-update
-base_bsp_version="R34-1"
+base_bsp_version="R32-7"
 target_release="35.1.0"
 base_release_tarball=jetson_linux_r${base_release}_aarch64.tbz2
-base_release_tarball_wget=https://developer.nvidia.com/embedded/l4t/r34_release_v1.1/release/${base_release_tarball}
+base_release_tarball_wget=https://developer.nvidia.com/embedded/l4t/r32_release_v7.2/t186/${base_release_tarball}
 base_release_rootfs_tarball=tegra_linux_sample-root-filesystem_r${base_release}_aarch64.tbz2
-base_release_rootfs_tarball_wget=https://developer.nvidia.com/embedded/l4t/r34_release_v1.1/release/${base_release_rootfs_tarball}
+base_release_rootfs_tarball_wget=https://developer.nvidia.com/embedded/l4t/r32_release_v7.2/t186/${base_release_rootfs_tarball}
 ota_tools_tarball=ota_tools_R${target_release}_aarch64.tbz2
 ota_tools_tarball_wget=https://developer.nvidia.com/embedded/L4T/r35_Release_v1.0/Release/${ota_tools_tarball}
 target_release_tarball=jetson_linux_r${target_release}_aarch64.tbz2
@@ -20,11 +20,11 @@ mkdir -p workdir/base
 mkdir -p workdir/target
 mkdir -p workdir/output
 set -e
-BASE_BSP=$(pwd)/workdir/base/Linux_for_Tegra
-TARGET_BSP=$(pwd)/workdir/target/Linux_for_Tegra
+export BASE_BSP=$(pwd)/workdir/base/Linux_for_Tegra
+export TARGET_BSP=$(pwd)/workdir/target/Linux_for_Tegra
 out_dir=$(pwd)/workdir/output
 dl_dir=~/nvidia/Downloads/nvidia/sdkm_downloads
-if [ ! -d ${BASE_BSP} ]; then
+if [ ! -e ${BASE_BSP}/apply_binaries.sh ]; then
     echo "${BASE_BSP} does not exist, extracting"
     if [ ! -e ${dl_dir}/${base_release_tarball} ]; then
         echo "${dl_dir}/${base_release_tarball} does not exist, downloading"
